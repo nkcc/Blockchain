c8e48650a364d52d204ae136ce915bcdc51260e5
alexcc <nkucc@outlook.com>
2019-10-13 21:02:35 +0800

    feat(genesisblock): add first create

---
+++ main.js
@@ -0,0 +1,35 @@
+const SHA256 = require('crypto-sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
+    }
+}
+
+class BlockChian {
+    constructor() {
+        this.chain = [];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '12/10/2019', 'Genesis Block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
